7ee3dc2936f5f53c613fa4584efff20eca689ca6
cemil.aydogdu <cemil.aydogdu@softtech.com.tr>
2019-01-18 15:37:11 +0300

    null control for adjustment

--- src/main/java/com/gameof3/util/GameUtil.java
+++ src/main/java/com/gameof3/util/GameUtil.java
@@ -19,7 +19,7 @@ public final class GameUtil {
 
     public static int getAdjustment(String adj, int number) {
         int adjustment = 0;
-        if (adj.equals("")) {
+        if ("".equals(adj)) {
             adjustment = GameUtil.getAdjustmentToDivide3(number);
         } else {
             adjustment = Integer.parseInt(adj);
